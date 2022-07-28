e151d6c83163b15c69556828f22d4253f841d72f
Maks Kozlov <108281855+Gladiator-Vlasiha@users.noreply.github.com>
2022-07-28 18:11:34 +0700

    Delete komandor directory

--- komandor/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.komandor {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.komandor to javafx.fxml;
-    exports com.example.komandor;
-}
